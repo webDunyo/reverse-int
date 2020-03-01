3069387fbf4fd73c6c415199c9b1553e06716816
Temurjon Sharifov <webdunyo@gmail.com>
2020-03-01 14:10:53 +0500

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
+	
+		let strNum = n.toString();
+
+		if(strNum[0] === '-'){
+			return strNum.slice(1).split('').reverse().join('');
+	
+		}
+
+	    return  strNum.toString().split('').reverse().join('');
+	
   
 }
